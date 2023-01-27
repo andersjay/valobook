321c7e2ca9ad6bf2b536d315ca3d70f3b020914f
Anderson Leite <anderson07102010@hotmail.com>
2023-01-27 13:10:26 -0300

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {Poppins} from '@next/font/google'
+import '@/styles/globals.css'
+
+
+const poppins = Poppins({subsets: ['latin'], weight: ['400','500','700']})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
